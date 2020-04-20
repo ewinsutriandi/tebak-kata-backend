f5e0045000d7818317761d7fe2b2f5fb7bfe5339
Ewin Sutriandi <ewin.sutriandi@gmail.com>
2020-04-20 16:21:07 +0800

    config

--- application/config.py
+++ application/config.py
@@ -7,10 +7,10 @@ class BaseConfig(object):
     DBUSER = 'root'
     DBPASS = 'root'
     DBNAME = 'wg'
-    #CORS_ORIGINS= ['http://localhost:8080','https://sidatu.lomboktengahkab.go.id']
+    #CORS_ORIGINS= ['http://localhost:8080','https://alza.web.id']
     #UPLOAD_FOLDER = str(os.path.dirname(os.path.abspath(__file__))+'/static/image')
     #UPLOAD_MAX_CTN_LEN= 5 * 1024 * 1024
-    #CLIENT_ID = '224603931516-rff9gibtotfm6mljj278gdet57pa012t.apps.googleusercontent.com'
+    #CLIENT_ID = '#'
     #JWT_BLACKLIST_ENABLED=True
     #JWT_BLACKLIST_TOKEN_CHECKS=['access','refresh']
 
